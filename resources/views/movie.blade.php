7faa3d45d3470f28a3b011da1e62e2be4f65279e
EnxhiHoxhaj <hoxhajenxhi1@gmail.com>
2024-09-11 18:49:08 +0200

    faet: ad database to table

--- resources/views/movie.blade.php
+++ resources/views/movie.blade.php
@@ -5,29 +5,29 @@
 <div class="container">
     <h1>Videoteca</h1>
     <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">ID</th>
-      <th scope="col">Title</th>
-      <th scope="col">Titolo originale</th>
-      <th scope="col">Nazionalità</th>
-      <th scope="col">Data d'uscita</th>
-      <th scope="col">Voto</th>
-    </tr>
-  </thead>
-  <tbody>
-
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-
-  </tbody>
-</table>
+    <thead>
+      <tr>
+        <th scope="col">ID</th>
+        <th scope="col">Title</th>
+        <th scope="col">Titolo originale</th>
+        <th scope="col">Nazionalità</th>
+        <th scope="col">Data d'uscita</th>
+        <th scope="col">Voto</th>
+      </tr>
+    </thead>
+    <tbody>
+      @foreach ($movie as $film)
+      <tr>
+        <th scope="row">{{$film->id}}</th>
+        <td>{{$film->title}}</td>
+        <td>{{$film->original_title}}</td>
+        <td>{{$film->nationality}}</td>
+        <td>{{$film->date}}</td>
+        <td>{{$film->vote}}</td>
+      </tr>
+      @endforeach
+    </tbody>
+  </table>
 </div>
 
 @endsection
